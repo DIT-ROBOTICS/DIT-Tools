7035625abcb20bb42951048448d7524177fa592d
SeanChangX <scx@gapp.nthu.edu.tw>
2025-03-26 06:00:59 +0800

    Enhance Docker setup with additional packages and SMB configuration in entrypoint script

--- Groot2/docker/entrypoint.sh
+++ Groot2/docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-# Set the ROS workspace
+# ==============================
+# ===== ROS Installation =======
+# ==============================
 if [ -z "$ROS_WS" ]; then
     echo "ROS_WS is not set. Setting to default"
     ROS_WS=/home/ros/app
@@ -10,6 +12,41 @@ cd $ROS_WS
 # . /opt/ros/humble/setup.sh && colcon build --symlink-install
 # source $ROS_WS/install/setup.bash
 
+
+# ==============================
+# ===== SMB Configuration ======
+# ==============================
+
+# List of host suffixes (dit-2025-11 ~ dit-2025-14)
+HOSTS=("11" "12" "13" "14")
+
+# Credentials for Samba
+SMB_USER="dit"
+SMB_PASS="robotics"
+
+# Base path for mount points
+MOUNT_BASE="$HOME/app/groot2/DIT"
+
+# Loop through each host and mount if reachable
+# Get the first 9 digits of the IP from the host network
+HOST_IP_PREFIX=$(hostname -I | awk '{print $1}' | cut -d. -f1-3)
+
+for i in "${HOSTS[@]}"; do
+    HOST_IP="${HOST_IP_PREFIX}.$i"
+    SHARE="//${HOST_IP}/dit"
+    MOUNT_POINT="$MOUNT_BASE/DIT-2025-$i"
+
+    echo -e "\e[1;34mChecking if $HOST_IP is online...\e[0m"
+
+    if ping -c 1 -W 1 $HOST_IP &> /dev/null; then
+        echo -e "\e[1;32m$HOST_IP is online. Mounting...\e[0m"
+        sudo mkdir -p "$MOUNT_POINT"
+        sudo mount -t cifs "$SHARE" "$MOUNT_POINT" \
+            -o username=$SMB_USER,password=$SMB_PASS,vers=3.0
+    else
+        echo -e "\e[1;31m$HOST_IP is offline. Skipping...\e[0m"
+    fi
+done
 
 
 # ==============================
